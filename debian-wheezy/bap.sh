300877a9c5f76c79d92c1d817e4a3f16b45bdf3b
Lukas Loesche <lloesche@gmail.com>
2015-02-12 17:10:41 +0100

    update compile flags

--- debian-wheezy/bap.sh
+++ debian-wheezy/bap.sh
@@ -13,7 +13,7 @@ export GOPATH=/build
 go get github.com/miekg/dns
 go get github.com/mesosphere/mesos-dns
 cd $GOPATH/src/github.com/mesosphere/mesos-dns
-go build -o mesos-dns main.go
+go build -o mesos-dns
 strip mesos-dns
 cp mesos-dns /package/root/usr/bin/
 cp config.json.sample /package/root/etc/mesos-dns/
